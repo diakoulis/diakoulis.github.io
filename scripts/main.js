0a63d491495bef159e3d51265c4a72b012188081
diakoulis <diakoulis.xaris@gmail.com>
2026-09-08 18:49:32 +0300

    First images

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-if (window.location.pathname.endsWith("index.html")) { 
-    title = "Home"
-} else {
-    title = "Error 404"
-}
-
-document.getElementById("page-title").textContent =  title + " | Diakoulis";
